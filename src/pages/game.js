4d469db1eae5e9c03da123765879b7650cf1c457
chris <chrisding03@gmail.com>
2019-04-13 14:09:23 +0800

    update game

---
+++ src/pages/game.js
@@ -0,0 +1,7 @@
+import React from "react"
+import { Link } from "gatsby"
+import Gam from "../components/game/gam"
+
+export default () => (
+  <Gam/>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,8 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+      <br/>
+    <Link to="/game/">Go to game</Link>
   </Layout>
 )
 
